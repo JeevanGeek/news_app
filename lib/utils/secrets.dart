761bf4ceca4c421e011d4add8b6f952f805ba28f
Jeevan Joshi <jeevanc@geekyants.com>
2022-07-12 16:14:56 +0530

    ADD: utils

---
+++ lib/utils/secrets.dart
@@ -0,0 +1,4 @@
+class API {
+  static const baseUrl = "https://newsapi.org/v2";
+  static const key = '9b1cacc3bf9441fb845bcb00287d0374';
+}
---
+++ lib/utils/sharedprefs.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage {
+  static late SharedPreferences preference;
+
+  static Future init() async {
+    preference = await SharedPreferences.getInstance();
+  }
+
+  static Future setCountry(String country) async {
+    await preference.setString("country", country);
+  }
+
+  static String getCountry() {
+    return preference.getString("country") ?? "";
+  }
+
+  static Future setCountryCode(String countryCode) async {
+    await preference.setString("countryCode", countryCode);
+  }
+
+  static String getCountryCode() {
+    return preference.getString("countryCode") ?? "";
+  }
+}
